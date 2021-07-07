48f9af83df6b91b4e4fac9646c2a34a99abf447e
Sergey <vismz@yandex.ru>
2021-07-07 12:08:22 +0700

    Bug fix

--- cli_sbercloud/cmd/ecs.go
+++ cli_sbercloud/cmd/ecs.go
@@ -14,6 +14,15 @@ var ecsCommand = &cobra.Command{
 	},
 }
 
+var StatisticSubcommand = &cobra.Command{
+	Use:   "statistic",
+	Short: "get statistic about servers",
+	Long: `Managing and getting statistics on Elastic Cloud Server`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hola, brooo :)")
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(ecsCommand)
 }
