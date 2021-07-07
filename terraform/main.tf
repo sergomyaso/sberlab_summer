940365761c6cd26c0b8ae073bf805f2810aac22c
Sergey <vismz@yandex.ru>
2021-07-07 14:31:08 +0700

    Add ecs template

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,17 @@
 terraform {
   required_version = ">= 0.12"
+  required_providers {
+    sbercloud = {
+        source = "sbercloud-terraform/sbercloud"
+        version = "1.3.0"
+        }
+  }
 }
 
+# Configure the SberCloud Provider
 provider "sbercloud" {
-      region     = "ru-moscow-1"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+    region = "ru-moscow-1"
+    access_key = ""
+    secret_key = ""
+    project_name = "summer_school"
 }
-
