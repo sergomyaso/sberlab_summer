81b5f691cb793d4cacb5ee0b1bd1d64bda8af4f1
Sergey <vismz@yandex.ru>
2021-07-21 10:21:59 +0700

    Add resource_worker

---
+++ pod_test_app/test_load.py
@@ -0,0 +1,66 @@
+import tempfile
+
+import yaml
+from kubernetes import client, config
+import os
+
+
+class DeployEntity:
+    def __init__(self, template_path, name, image, mem_limit, cpu_limit, replicas=1, namespace="default", port=8080):
+        self.template_path = template_path
+        self.name = name
+        self.image = image
+        self.mem_limit = mem_limit
+        self.cpu_limit = cpu_limit
+        self.replicas = replicas
+        self.namespace = namespace
+        self.port = port
+
+    def get_render_template(self):
+        with open(self.template_path) as template_file:
+            string_template = template_file.read()
+            return eval(f'f"""{string_template}"""')
+
+
+class ResourceHandler:
+    __TEMP_PATH = "./temp"
+
+    def __init__(self, config_path):
+        self.__config_path = config_path
+
+    def __config_client(self):
+        config.load_kube_config(config_file=self.__config_path)
+
+    @staticmethod
+    def __create_resource_yaml(resource_entity):
+        file = tempfile.TemporaryFile(suffix=".yaml", dir=ResourceHandler.__TEMP_PATH, delete=False)
+        script = resource_entity.get_render_template()
+        file.write(bytes(script.encode()))
+        file.close()
+        return file.name
+
+    def apply_deployment(self, resource_entity):
+        self.__config_client()
+        resource_name = self.__create_resource_yaml(resource_entity)
+        print(resource_name)
+        with open(os.path.join(os.path.dirname(__file__), resource_name)) as f:
+            dep = yaml.safe_load(f)
+            k8s_apps_v1 = client.AppsV1Api()
+            k8s_apps_v1.create_namespaced_deployment(
+                body=dep, namespace=resource_entity.namespace)
+
+            print(f"\n[INFO] deployment {resource_entity.name} created.")
+        os.remove(resource_name)
+
+    def remove_deployment(self, resource_entity):
+        self.__config_client()
+        k8s_apps_v1 = client.AppsV1Api()
+        k8s_apps_v1.delete_namespaced_deployment(name=resource_entity.name, namespace=resource_entity.namespace)
+        print(f"\n[INFO] deployment {resource_entity.name} deleted.")
+
+
+if __name__ == '__main__':
+    entity = DeployEntity("template/DeployTemplate.yaml", "java-http", "sergomyaso/java-http", "50Mi", 1)
+    handler = ResourceHandler("configs/myaso-config.yaml")
+    #handler.apply_deployment(entity)
+    #handler.remove_deployment(entity)
